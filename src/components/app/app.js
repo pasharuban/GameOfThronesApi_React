bdd800421912ead8fc640e9dd2e850a42762840d
Pavel <pavelruban@bk.ru>
2021-09-28 15:45:17 +0300

    added a button to show/hide randomChar Component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,77 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
-import Header from '../header';
-import RandomChar from '../randomChar';
-import ItemList from '../itemList';
-import CharDetails from '../charDetails';
+import React, { Component } from "react";
+import { Col, Row, Container } from "reactstrap";
+import Header from "../header";
+import RandomChar from "../randomChar";
+import ItemList from "../itemList";
+import CharDetails from "../charDetails";
+import styled from "styled-components";
 
+const ButtonForRandomChar = styled.button`
+  width: 120px;
+  padding: 5px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 15px;
+  color: white;
+  background-color: teal;
+  border: 1px solid black;
+  border-radius: 2px;
+  margin-bottom: 10px;
+`;
 
-const App = () => {
+export default class App extends Component {
+  state = {
+    showRandomChar: true,
+    buttonText: "Hide",
+  };
+
+  onRandomChar = () => {
+    this.setState(({ showRandomChar }) => {
+      const buttonText = showRandomChar ? "Show" : "Hide";
+      return {
+        showRandomChar: !showRandomChar,
+        buttonText: buttonText,
+      };
+    });
+  };
+
+  showRandomChar() {
+    if (this.state.showRandomChar) {
+      return <RandomChar />;
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    const elem = this.showRandomChar();
     return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
+      <>
+        <Container>
+          <Header />
+        </Container>
+        <Container>
+          <Row>
+            <Col lg={{ size: 5, offset: 0 }}>
+              <div className="random-char-section">
+                <ButtonForRandomChar onClick={this.onRandomChar}>
+                  {this.state.buttonText}
+                </ButtonForRandomChar>
+                {elem}
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="6">
+              <ItemList />
+            </Col>
+            <Col md="6">
+              <CharDetails />
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
-};
-
-export default App;
+  }
+}
